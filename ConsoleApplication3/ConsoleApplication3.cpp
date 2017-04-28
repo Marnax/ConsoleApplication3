e2a9fe93ff564170f4d169d191065f65a4612326
Matthias Zappe <marnax@gmail.com>
2017-04-28 17:32:50 +0200

    Lockingtest

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,11 +1,33 @@
-// ConsoleApplication3.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
-//
-
-#include "stdafx.h"
-
+#include <stdafx.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
 int main()
 {
-    return 0;
-}
+	std::mutex m;
+	std::condition_variable v;
+	std::thread t1([&m]() {
+		while (true)     
+		{
+			std::unique_lock<std::mutex> lock(m);
+			std::cout << "T1 got the lock! " << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(3));
+			std::cout << "T1 will lose the lock!" << std::endl;
+		}
+	});
+	std::thread t2([&m]() {
+		while (true)
+		{
+			std::unique_lock<std::mutex> lock(m);
+			std::cout << "T2 got the lock! " << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+			std::cout << "T2 will lose the lock!" << std::endl;
+		}
+	});
 
+	t1.join();
+	t2.join();
+	return 0;
+}
